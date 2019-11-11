7442282ef8875752a62e3350e9cb1704e755c59e
xiaowu <550200360@qq.com>
2019-11-11 14:57:42 +0800

    feat: transform request data
    transform request data

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'post'
+  | 'POST'
+  | 'delete'
+  | 'Delete'
+  | 'options'
+  | 'OPTIONS'
+  | 'head'
+  | 'HEAD'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
