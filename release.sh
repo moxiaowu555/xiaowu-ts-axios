dc8e25e769698e42bef0d4f9a3e2b2db03d9b522
xiaowu <550200360@qq.com>
2019-12-06 10:06:05 +0800

    nothing changed

--- release.sh
+++ release.sh
@@ -2,8 +2,8 @@
 set -e
 echo "Enter release version: "
 read VERSION
-read -p "Releasing #VERSION - are you sure? (y/n)" -n 1 -r
-echo # (optional) move to a new line
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo  # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   echo "Releasing $VERSION ..."
